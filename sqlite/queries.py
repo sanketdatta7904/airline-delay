2d3e5fbc82321719bfac3258c034032ff5cde637
rentschler <73845942+rentschler@users.noreply.github.com>
2024-01-10 16:12:13 +0100

    added comments describing the queries, and added some meta files to git(??) (jonathan and sanket)

--- sqlite/queries.py
+++ sqlite/queries.py
@@ -3,11 +3,12 @@ import time
 
 conn = sqlite3.connect('D:/sqlite/aviation.db')
 
-
+# just the flights data
 query_for_filghts_data = "SELECT adep_name, ADEP Latitude, ADEP Longitude, ades_name, ADES Latitude, ADES Longitude, `delay (minutes)` FROM flights_data ORDER BY `delay (minutes)` DESC;" 
 
+# db to get the aggregated delays, additionally containing the type (small_airport, medium_airport, large_airport, heliport, closed, seaplane_base)
 query_for_aggregated_delays = "SELECT ades_type, AVG(`avg_delay`) AS avg_delay FROM aggregated_delays GROUP BY ades_type;" 
-
+# delay of a specific route
 query_for_route_delays = """SELECT RouteID, SUM(delay) as TotalDelay
 FROM route_delay
 WHERE Date BETWEEN '2015-03-01' AND '2018-09-01'
